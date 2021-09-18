c1ac8068412336c63ada31cd3098b1d6e8cc568a
Aditya Nagar <nagar.s.aditya@gmail.com>
2021-09-18 12:43:22 +0530

    Profile page added

---
+++ MoneyO/src/pages/ProfilePage/ProfilePage.js
@@ -0,0 +1,23 @@
+import './ProfilePage.css'
+import RectangleOne from '../../assets/ProfilePage/RectangleOne.png'
+import RectangleTwo from '../../assets/ProfilePage/RectangleTwo.png'
+import DefaultImage from '../../assets/ProfilePage/defaultimage.png'
+
+
+const ProfilePage = () => {
+
+    return(
+        <div className="ProfilePageBody">
+            <span className="ProfilePageTitle">My Profile</span><br />
+           <div className=" row">
+            <div className=" col-lg-6">
+                <div className="ProfilePageLeft">
+                <img className="ProfilePicture" src={DefaultImage} />
+                </div>
+            </div>
+           </div>
+        </div>
+    )
+}
+
+export default ProfilePage
